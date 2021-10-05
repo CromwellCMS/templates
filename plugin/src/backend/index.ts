807d8a12d534a6cd43b28fd14278c4c2b4ac8307
eilrix <41447729+eilrix@users.noreply.github.com>
2021-10-06 00:58:37 +0300

    fix action type

--- plugin/src/backend/index.ts
+++ plugin/src/backend/index.ts
@@ -5,7 +5,7 @@ import { Controller, Get } from '@nestjs/common';
 registerAction({
   pluginName: 'your-plugin-name',
   actionName: 'install_plugin',
-  action: (payload) => {
+  action: async (payload) => {
     if (payload.pluginName === 'your-plugin-name') {
       getLogger().info('Thanks for installing our plugin!');
     }
